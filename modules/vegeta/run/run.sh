4f19fd1634440faf839b7b53cdbfd82e3d1ab95c
kingoliver@microsoft.com <kingoliver@microsoft.com>
2025-06-30 15:55:13 -0400

    add vegeta run

---
+++ modules/vegeta/run/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Script to run Vegeta HTTP load testing attacks
+# https://github.com/tsenart/vegeta
+
+set -e
+
+function run_vegeta_attack() {
+    local target_url="${1}"
+    if [ -z "$target_url" ]; then
+        echo "Usage: $0 <target_url> [rate] [duration] [workers]"
+        echo "Example: ./modules/vegeta/run/run.sh http://localhost:8080 50 30s 10"
+        return 1
+    fi
+    local rate="${2:-50}"        # requests per second, default 50
+    local duration="${3:-30s}"   # duration of test, default 30s
+    local workers="${4:-10}"     # number of workers, default 10
+
+    echo "Running Vegeta attack with:"
+    echo "- Target URL: $target_url"
+    echo "- Rate: $rate requests/second"
+    echo "- Duration: $duration"
+    echo "- Workers: $workers"
+
+
+    # Run attack and generate report
+    echo "GET $target_url" | \
+    vegeta attack \
+        -rate=$rate \
+        -duration=$duration \
+        -workers=$workers | \
+    vegeta encode
+}
+
+# If script is run directly (not sourced)
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    run_vegeta_attack "$@"
+fi
