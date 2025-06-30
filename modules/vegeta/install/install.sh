dc066b4991de44424609e782458ec14e942f0ecc
kingoliver@microsoft.com <kingoliver@microsoft.com>
2025-06-30 16:59:02 -0400

    add jaggr install

--- modules/vegeta/install/install.sh
+++ modules/vegeta/install/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Script to install Vegeta HTTP load testing tool
+# Script to install Vegeta HTTP load testing tool and jaggr
 # https://github.com/tsenart/vegeta
+# https://github.com/rs/jaggr
 
 set -e
 
+# Install Vegeta
 VEGETA_VERSION="12.12.0"
 ARCH=$(uname -m)
 
@@ -22,7 +24,7 @@ case ${ARCH} in
         ;;
 esac
 
-# Download URL
+# Download URL for Vegeta
 DOWNLOAD_URL="https://github.com/tsenart/vegeta/releases/download/v${VEGETA_VERSION}/vegeta_${VEGETA_VERSION}_linux_${ARCH}.tar.gz"
 
 echo "Downloading Vegeta v${VEGETA_VERSION} for Linux ${ARCH}..."
@@ -35,10 +37,31 @@ echo "Installing Vegeta to /usr/local/bin..."
 sudo mv vegeta /usr/local/bin/
 rm vegeta.tar.gz
 
-echo "Setting executable permissions..."
+echo "Setting executable permissions for Vegeta..."
 sudo chmod +x /usr/local/bin/vegeta
 
-echo "Verifying installation..."
+echo "Verifying Vegeta installation..."
 vegeta --version
 
-echo "Vegeta installation complete!"
+# Install jaggr
+JAGGR_VERSION="1.0.0"
+JAGGR_URL="https://github.com/rs/jaggr/releases/download/${JAGGR_VERSION}/jaggr_${JAGGR_VERSION}_linux_${ARCH}.tar.gz"
+
+echo "Downloading jaggr v${JAGGR_VERSION} for Linux ${ARCH}..."
+wget -q "${JAGGR_URL}" -O jaggr.tar.gz
+
+echo "Extracting jaggr..."
+tar xzf jaggr.tar.gz jaggr
+
+echo "Installing jaggr to /usr/local/bin..."
+sudo mv jaggr /usr/local/bin/
+rm jaggr.tar.gz
+
+echo "Setting executable permissions for jaggr..."
+sudo chmod +x /usr/local/bin/jaggr
+
+echo "Installation complete!"
+echo "Installed tools:"
+echo "- Vegeta: $(vegeta --version)"
+echo "- jaggr: "
+jaggr --version
